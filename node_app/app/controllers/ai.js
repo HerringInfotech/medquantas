af81ca47c81cde05a2f6fc399f1b5293c05c074f
sathya <satheeshkumar@herringinfotech.com>
2026-06-12 16:30:47 +0530

    airport filter

--- node_app/app/controllers/ai.js
+++ node_app/app/controllers/ai.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+require('dotenv').config({
+  path: require('path').resolve(__dirname, '../.env')
+});
 const Groq = require('groq-sdk');
 const ItemMaster = require('../models/item_master');
 const PriceMaster = require('../models/price_master');
